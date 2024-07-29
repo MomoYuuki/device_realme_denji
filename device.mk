f7e59dbbb770eccc9906c86a9ba71c8a55a76eed
MomoYuuki <2711300270@qq.com>
2024-07-29 17:17:08 +0800

    denji:nuke pixel

--- device.mk
+++ device.mk
@@ -397,8 +397,6 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/google/interfaces \
-    hardware/google/pixel \
     hardware/mediatek \
     hardware/oplus
 
