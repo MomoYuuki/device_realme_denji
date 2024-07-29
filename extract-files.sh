12cbf81249a03e096fd53dcf7a2a3abe925c6cd6
MomoYuuki <2711300270@qq.com>
2024-07-29 17:01:40 +0800

    denji:use prebuilt kernel for now

--- extract-files.sh
+++ extract-files.sh
@@ -53,43 +53,9 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
-function blob_fixup {
-    case "$1" in
-	system_ext/lib64/libsink.so)
-            "${PATCHELF}" --add-needed "libshim_sink.so" "$2"
-            ;;
-        vendor/lib*/hw/vendor.mediatek.hardware.pq@2.13-impl.so)
-            "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
-            ;;
-	vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service)
-            "$PATCHELF" --replace-needed "android.hardware.power-V2-ndk_platform.so" "android.hardware.power-V2-ndk.so" "$2"
-            ;;
-	vendor/bin/hw/android.hardware.gnss-service.mediatek | \
-        vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
-            "$PATCHELF" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "$2"
-            ;;
-	vendor/bin/hw/android.hardware.memtrack-service.mediatek)
-            "$PATCHELF" --replace-needed "android.hardware.memtrack-V1-ndk_platform.so" "android.hardware.memtrack-V1-ndk.so" "$2"
-            ;;
-        vendor/etc/init/vendor.mediatek.hardware.mtkpower@1.0-service.rc)
-            echo "$(cat ${2}) input" > "${2}"
-            ;;
-        vendor/etc/init/android.hardware.neuralnetworks@1.3-service-mtk-neuron.rc)
-            sed -i 's/start/enable/' "$2"
-            ;;
-	vendor/lib*/libmtkcam_stdutils.so)
-            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "$2"
-            ;;
-	vendor/etc/vintf/manifest/manifest_media_c2_V1_2_default.xml)
-            sed -i 's/1.1/1.2/' "$2"
-            ;;
-	vendor/bin/hw/android.hardware.media.c2@1.2-mediatek)
-            "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
-            ;;
-	vendor/bin/mnld | \
-	vendor/lib*/libaalservice.so | \
-	vendor/lib64/libcam.utils.sensorprovider.so)
-            "${PATCHELF}" --add-needed "libshim_sensors.so" "${2}"
+function blob_fixup() {
+    case "${1}" in
+        *)
             ;;
     esac
 }
